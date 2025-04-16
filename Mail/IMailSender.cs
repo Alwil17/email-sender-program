361a89028bb255e30a1370570e1b50d29ce47f81
Alwil17 <willialfred24@gmail.com>
2025-04-16 00:28:26 +0000

    Added MailSender interface

---
+++ Mail/IMailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSenderProgram.Mail
+{
+    internal interface IMailSender
+    {
+    }
+}
